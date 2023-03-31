d020b5a303499272c667d5806517fc6ddf9ba3f3
Tomno <tomnoviv@gmail.com>
2023-03-31 12:41:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ function moveDodgerLeft() {
     }
   }
 
-  const GAME_WIDTH = 400;
-  const DODGER_WIDTH = 40;
-
-function positionToInteger(position) {
-  return parseInt(position.split('px')[0]) || 0;
-}
-
-function moveDodgerRight() {
-  const rightEdge = GAME_WIDTH - DODGER_WIDTH;
-  const left = positionToInteger(DODGER.style.left);
-
-  if (left < rightEdge) {
-    DODGER.style.left = `${left + 4}px`;
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+  if (left < 360) { 
+    dodger.style.left = `${left + 1}px`;
   }
-}
+
+  }
